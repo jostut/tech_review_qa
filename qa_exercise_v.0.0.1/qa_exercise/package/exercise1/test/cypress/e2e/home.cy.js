e0ad6aa5997f4eaedcbf4aa095d1428651d6ad94
jostut <JGStutson@gmail.com>
2024-09-04 19:19:12 -0600

    started first test

--- qa_exercise_v.0.0.1/qa_exercise/package/exercise1/test/cypress/e2e/home.cy.js
+++ qa_exercise_v.0.0.1/qa_exercise/package/exercise1/test/cypress/e2e/home.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Home Page', () => {
+  afterEach(()=>{
+    cy.request('/account/signout')
+  })
+  describe('Login', () =>{
+    it('is able to login', () => {
+      cy.visit('home.html')
+    })
   })
 })
